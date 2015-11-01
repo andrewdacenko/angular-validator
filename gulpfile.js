6a77c8f2f4fd24a85f750da52acdc21f67e0d336
Andrew Dacenko <a.s.dvornik@gmail.com>
2015-11-02 01:32:42 +0200

    update(gulpfile) improved test start and build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var pkg = require('./package.json');
 var gulp = require('gulp');
-var del = require('del');
 var wrap = require('gulp-wrap');
+var rimraf = require('rimraf');
+var gulpIf = require('gulp-if');
 var concat = require('gulp-concat');
 var header = require('gulp-header');
 var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
 
 var banner = [
@@ -16,52 +19,67 @@ var banner = [
     ' */\n'
 ].join('\n');
 
-gulp.task('clean', function (cb) {
-    del(['dist/*.js'], cb);
+var module = {
+    header: 'angular.module(\'ngValidator\', [\'ngValidator.message-bag\', \'ngValidator.validation-translator\', \'ngValidator.validator\']);\n\n',
+    wrap: ';(function (window, angular, undefined) {\n\'use strict\';\n<%= contents %>\n})(window, angular);'
+};
+
+var scripts = ['src/**/*.module.js', 'src/**/!(*.spec).js'];
+
+gulp.task('clean', function(cb) {
+    rimraf('dist', cb);
 });
 
-gulp.task('scripts', ['clean'], function () {
-    return gulp.src(['src/**/*.js', '!src/**/test/*.spec.js'])
-        .pipe(concat(pkg.name + '.js'))
-        .pipe(header('angular.module(\'ngValidator\', [\'ngValidator.message-bag\', \'ngValidator.validation-translator\', \'ngValidator.validator\']);\n\n'))
-        .pipe(wrap(';(function (window, angular, undefined) {\n"use strict";\n<%= contents %>\n})(window, angular);'))
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
+gulp.task('copy-bower', function() {
+    return gulp.src('bower.json')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('uglify', ['clean'], function () {
-    return gulp.src(['src/**/*.js', '!src/**/test/*.spec.js'])
-        .pipe(sourcemaps.init())
-        .pipe(concat(pkg.name + '.min.js'))
-        .pipe(header('angular.module(\'ngValidator\', [\'ngValidator.message-bag\', \'ngValidator.validation-translator\', \'ngValidator.validator\']);\n\n'))
-        .pipe(wrap(';(function (window, angular, undefined) {\n"use strict";\n<%= contents %>\n})(window, angular);'))
-        .pipe(uglify())
+gulp.task('scripts', ['clean'], buildScripts);
+
+gulp.task('uglify', ['clean'], buildScripts.bind(null, true));
+
+function buildScripts() {
+    var minify = (arguments.length === 2);
+
+    return gulp.src(scripts)
+        .pipe(plumber())
+        .pipe(gulpIf(minify, sourcemaps.init()))
+        .pipe(concat(pkg.name + (minify ? '.min' : '') + '.js'))
+        .pipe(header(module.header))
+        .pipe(wrap(module.wrap))
+        .pipe(gulpIf(minify, ngAnnotate()))
+        .pipe(gulpIf(minify, uglify()))
         .pipe(header(banner, {
             pkg: pkg
         }))
-        .pipe(sourcemaps.write('./'))
+        .pipe(gulpIf(minify, sourcemaps.write('./')))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('test', function (done) {
-    require('karma').server.start({
+gulp.task('test', runKarma.bind(null, true));
+
+gulp.task('tdd', runKarma);
+
+function runKarma(done) {
+    var singleRun = false;
+
+    if(arguments.length === 2) {
+        done = arguments[1];
+        singleRun = true;
+    }
+
+    var Server = require('karma').Server;
+    var server = new Server({
         configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, function () {
+        singleRun: singleRun
+    }, function() {
         done();
     });
-});
 
-gulp.task('tdd', function (done) {
-    require('karma').server.start({
-        configFile: __dirname + '/karma.conf.js',
-    }, function () {
-        done();
-    });
-});
+    server.start();
+}
 
-gulp.task('build', ['scripts', 'uglify']);
+gulp.task('build', ['scripts', 'uglify', 'copy-bower']);
 
 gulp.task('default', ['build']);
